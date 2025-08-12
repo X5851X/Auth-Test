599c96082bb38fc52e293cc4e0a734f69cabd52d
Risfa <risfandhiani@gmail.com>
2025-08-12 21:02:38 +0700

    crypto apa?

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { webcrypto } from 'crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as any;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
